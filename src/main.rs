9692bc6050ef6351bf43a1817ed64820dd7c0c5b
Luca <lucaan@ethz.ch>
2023-10-06 23:40:50 +0200

    added map rendering

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use std::collections::HashMap;
+
 use quicksilver::{
     geom::Vector,
     lifecycle::{run, Settings, State, Window},
-    prelude::{Asset, Background::Img, Color, Font, FontStyle, Image, Shape},
+    prelude::{
+        Asset,
+        Background::{Blended, Img},
+        Color, Font, FontStyle, Image, Rectangle, Shape,
+    },
     Future, Result,
 };
 
 struct Game {
     title: Asset<Image>,
     mononoki_font_info: Asset<Image>,
+    map_size: Vector,
+    map: Vec<Tile>,
+    entities: Vec<Entity>,
+    player_id: usize,
+    tileset: Asset<HashMap<char, Image>>,
+    tile_size_px: Vector,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -20,7 +32,7 @@ struct Tile {
 struct Entity {
     pos: Vector,
     glyph: char,
-    color: Color
+    color: Color,
     hp: i32,
     max_hp: i32,
 }
@@ -46,6 +58,39 @@ fn generate_map(size: Vector) -> Vec<Tile> {
     map
 }
 
+fn generate_entities() -> Vec<Entity> {
+    vec![
+        Entity {
+            pos: Vector::new(9, 6),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(2, 4),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(7, 5),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+        Entity {
+            pos: Vector::new(4, 8),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+    ]
+}
+
 impl State for Game {
     fn new() -> Result<Self> {
         let font_mononoki = "mononoki-Regular.ttf";
@@ -61,9 +106,43 @@ impl State for Game {
             )
         }));
 
+        let map_size = Vector::new(20, 15);
+        let map = generate_map(map_size);
+        let mut entities = generate_entities();
+
+        let player_id = entities.len();
+        entities.push(Entity {
+            pos: Vector::new(5, 3),
+            glyph: '@',
+            color: Color::BLUE,
+            hp: 3,
+            max_hp: 5,
+        });
+
+        let game_glyphs = "#@g.%";
+        let tile_size_px = Vector::new(12, 24);
+        let tileset = Asset::new(Font::load(font_mononoki).and_then(move |text| {
+            let tiles = text
+                .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
+                .expect("Could not render the font tileset.");
+            let mut tileset = HashMap::new();
+            for (index, glyph) in game_glyphs.chars().enumerate() {
+                let pos = (index as i32 * tile_size_px.x as i32, 0);
+                let tile = tiles.subimage(Rectangle::new(pos, tile_size_px));
+                tileset.insert(glyph, tile);
+            }
+            Ok(tileset)
+        }));
+
         Ok(Self {
             title,
             mononoki_font_info,
+            map_size,
+            map,
+            entities,
+            player_id,
+            tileset,
+            tile_size_px,
         })
     }
 
@@ -91,6 +170,22 @@ impl State for Game {
                     .translate((2, window.screen_size().y as i32 - 60)),
                 Img(&image),
             );
+            Ok(())
+        })?;
+
+        let tile_size_px = self.tile_size_px;
+
+        let (tileset, map) = (&mut self.tileset, &self.map);
+        tileset.execute(|tileset| {
+            for tile in map.iter() {
+                if let Some(image) = tileset.get(&tile.glyph) {
+                    let pos_px = tile.pos.times(tile_size_px);
+                    window.draw(
+                        &Rectangle::new(pos_px, image.area().size()),
+                        Blended(&image, tile.color),
+                    );
+                }
+            }
             Ok(())
         })?;
 
