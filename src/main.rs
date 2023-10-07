9aeba6043cd12a01262012b3116675f57e6a5198
Luca <lucaan@ethz.ch>
2023-10-07 21:25:20 +0200

    updated to newest quicksilver version

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 use std::collections::HashMap;
 
 use quicksilver::{
-    geom::{Shape, Vector},
-    graphics::{Background::Img, Color, Font, FontStyle, Image},
-    lifecycle::{run, Asset, Settings, State, Window},
-    prelude::{
-        Background::{Blended, Col},
-        Key, Rectangle,
-    },
-    Future, Result,
+    geom::Vector,
+    graphics::{Color, Image, VectorFont},
+    run, Graphics, Input, Result, Settings, Window,
 };
 
 struct Game {
-    title: Asset<Image>,
-    mononoki_font_info: Asset<Image>,
-    square_font_info: Asset<Image>,
+    title: Image,
+    mononoki_font_info: Image,
+    square_font_info: Image,
     map_size: Vector,
     map: Vec<Tile>,
     entities: Vec<Entity>,
     player_id: usize,
-    tileset: Asset<HashMap<char, Image>>,
+    tileset: HashMap<char, Image>,
     tile_size_px: Vector,
 }
 
@@ -62,28 +57,28 @@ fn generate_map(size: Vector) -> Vec<Tile> {
 fn generate_entities() -> Vec<Entity> {
     vec![
         Entity {
-            pos: Vector::new(9, 6),
+            pos: Vector::new(9.0, 6.0),
             glyph: 'g',
             color: Color::RED,
             hp: 1,
             max_hp: 1,
         },
         Entity {
-            pos: Vector::new(2, 4),
+            pos: Vector::new(2.0, 4.0),
             glyph: 'g',
             color: Color::RED,
             hp: 1,
             max_hp: 1,
         },
         Entity {
-            pos: Vector::new(7, 5),
+            pos: Vector::new(7.0, 5.0),
             glyph: '%',
             color: Color::PURPLE,
             hp: 0,
             max_hp: 0,
         },
         Entity {
-            pos: Vector::new(4, 8),
+            pos: Vector::new(4.0, 8.0),
             glyph: '%',
             color: Color::PURPLE,
             hp: 0,
@@ -92,179 +87,198 @@ fn generate_entities() -> Vec<Entity> {
     ]
 }
 
-impl State for Game {
-    fn new() -> Result<Self> {
-        let font_mononoki = "mononoki-Regular.ttf";
-        let font_square = "square.ttf";
+// impl State for Game {
+//     fn new() -> Result<Self> {
+//         let font_mononoki = "mononoki-Regular.ttf";
+//         let font_square = "square.ttf";
 
-        let title = Asset::new(Font::load(font_mononoki).and_then(|font| {
-            font.render("Quicksilver Roguelike", &FontStyle::new(72.0, Color::BLACK))
-        }));
+//         let title = Asset::new(Font::load(font_mononoki).and_then(|font| {
+//             font.render("Quicksilver Roguelike", &FontStyle::new(72.0, Color::BLACK))
+//         }));
 
-        let mononoki_font_info = Asset::new(Font::load(font_mononoki).and_then(|font| {
-            font.render(
-                "Mononoki font by Matthias Tellen, terms: SIL Open Font License 1.1",
-                &FontStyle::new(20.0, Color::BLACK),
-            )
-        }));
+//         let mononoki_font_info = Asset::new(Font::load(font_mononoki).and_then(|font| {
+//             font.render(
+//                 "Mononoki font by Matthias Tellen, terms: SIL Open Font License 1.1",
+//                 &FontStyle::new(20.0, Color::BLACK),
+//             )
+//         }));
 
-        let square_font_info = Asset::new(Font::load(font_square).and_then(|font| {
-            font.render(
-                "Square font by Wouter Van Oortmerssen, terms: CC BY 3.0",
-                &FontStyle::new(20.0, Color::BLACK),
-            )
-        }));
+//         let square_font_info = Asset::new(Font::load(font_square).and_then(|font| {
+//             font.render(
+//                 "Square font by Wouter Van Oortmerssen, terms: CC BY 3.0",
+//                 &FontStyle::new(20.0, Color::BLACK),
+//             )
+//         }));
 
-        let map_size = Vector::new(20, 15);
-        let map = generate_map(map_size);
-        let mut entities = generate_entities();
+//         let map_size = Vector::new(20, 15);
+//         let map = generate_map(map_size);
+//         let mut entities = generate_entities();
 
-        let player_id = entities.len();
-        entities.push(Entity {
-            pos: Vector::new(5, 3),
-            glyph: '@',
-            color: Color::BLUE,
-            hp: 3,
-            max_hp: 5,
-        });
+//         let player_id = entities.len();
+//         entities.push(Entity {
+//             pos: Vector::new(5, 3),
+//             glyph: '@',
+//             color: Color::BLUE,
+//             hp: 3,
+//             max_hp: 5,
+//         });
 
-        let game_glyphs = "#@g.%";
-        let tile_size_px = Vector::new(24, 24);
-        let tileset = Asset::new(Font::load(font_square).and_then(move |text| {
-            let tiles = text
-                .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
-                .expect("Could not render the font tileset.");
-            let mut tileset = HashMap::new();
-            for (index, glyph) in game_glyphs.chars().enumerate() {
-                let pos = (index as i32 * tile_size_px.x as i32, 0);
-                let tile = tiles.subimage(Rectangle::new(pos, tile_size_px));
-                tileset.insert(glyph, tile);
-            }
-            Ok(tileset)
-        }));
+//         let game_glyphs = "#@g.%";
+//         let tile_size_px = Vector::new(24, 24);
+//         let tileset = Asset::new(Font::load(font_square).and_then(move |text| {
+//             let tiles = text
+//                 .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
+//                 .expect("Could not render the font tileset.");
+//             let mut tileset = HashMap::new();
+//             for (index, glyph) in game_glyphs.chars().enumerate() {
+//                 let pos = (index as i32 * tile_size_px.x as i32, 0);
+//                 let tile = tiles.subimage(Rectangle::new(pos, tile_size_px));
+//                 tileset.insert(glyph, tile);
+//             }
+//             Ok(tileset)
+//         }));
 
-        Ok(Self {
-            title,
-            mononoki_font_info,
-            square_font_info,
-            map_size,
-            map,
-            entities,
-            player_id,
-            tileset,
-            tile_size_px,
-        })
-    }
+//         Ok(Self {
+//             title,
+//             mononoki_font_info,
+//             square_font_info,
+//             map_size,
+//             map,
+//             entities,
+//             player_id,
+//             tileset,
+//             tile_size_px,
+//         })
+//     }
 
-    fn update(&mut self, window: &mut Window) -> Result<()> {
-        use quicksilver::input::ButtonState::*;
-        let player = &mut self.entities[self.player_id];
-        if window.keyboard()[Key::Left] == Pressed {
-            player.pos.x -= 1.0;
-        }
-        if window.keyboard()[Key::Right] == Pressed {
-            player.pos.x += 1.0;
-        }
-        if window.keyboard()[Key::Up] == Pressed {
-            player.pos.y -= 1.0;
-        }
-        if window.keyboard()[Key::Down] == Pressed {
-            player.pos.y += 1.0;
-        }
-        if window.keyboard()[Key::Escape].is_down() {
-            window.close();
-        }
-        Ok(())
-    }
+//     fn update(&mut self, window: &mut Window) -> Result<()> {
+//         let player = &mut self.entities[self.player_id];
+//         if window.keyboard()[Key::Left].is_down() {
+//             player.pos.x -= 1.0;
+//         }
+//         if window.keyboard()[Key::Right].is_down() {
+//             player.pos.x += 1.0;
+//         }
+//         if window.keyboard()[Key::Up].is_down() {
+//             player.pos.y -= 1.0;
+//         }
+//         if window.keyboard()[Key::Down].is_down() {
+//             player.pos.y += 1.0;
+//         }
+//         if window.keyboard()[Key::Escape].is_down() {
+//             window.close();
+//         }
+//         Ok(())
+//     }
 
-    fn draw(&mut self, window: &mut Window) -> Result<()> {
-        window.clear(Color::WHITE)?;
+//     fn draw(&mut self, window: &mut Window) -> Result<()> {
+//         window.clear(Color::WHITE)?;
 
-        self.title.execute(|image| {
-            window.draw(
-                &image
-                    .area()
-                    .with_center((window.screen_size().x as i32 / 2, 40)),
-                Img(&image),
-            );
-            Ok(())
-        })?;
+//         self.title.execute(|image| {
+//             window.draw(
+//                 &image
+//                     .area()
+//                     .with_center((window.screen_size().x as i32 / 2, 40)),
+//                 // Img(&image),
+//             );
+//             Ok(())
+//         })?;
 
-        self.mononoki_font_info.execute(|image| {
-            window.draw(
-                &image
-                    .area()
-                    .translate((2, window.screen_size().y as i32 - 60)),
-                Img(&image),
-            );
-            Ok(())
-        })?;
+//         self.mononoki_font_info.execute(|image| {
+//             window.draw(
+//                 &image
+//                     .area()
+//                     .translate((2, window.screen_size().y as i32 - 60)),
+//                 // Img(&image),
+//             );
+//             Ok(())
+//         })?;
 
-        self.square_font_info.execute(|image| {
-            window.draw(
-                &image
-                    .area()
-                    .translate((2, window.screen_size().y as i32 - 30)),
-                Img(&image),
-            );
-            Ok(())
-        })?;
+//         self.square_font_info.execute(|image| {
+//             window.draw(
+//                 &image
+//                     .area()
+//                     .translate((2, window.screen_size().y as i32 - 30)),
+//                 // Img(&image),
+//             );
+//             Ok(())
+//         })?;
 
-        let tile_size_px = self.tile_size_px;
-        let offset_px = Vector::new(50, 120);
-        let (tileset, map) = (&mut self.tileset, &self.map);
-        tileset.execute(|tileset| {
-            for tile in map.iter() {
-                if let Some(image) = tileset.get(&tile.glyph) {
-                    let pos_px = tile.pos.times(tile_size_px);
-                    window.draw(
-                        &Rectangle::new(pos_px + offset_px, image.area().size()),
-                        Blended(&image, tile.color),
-                    );
-                }
-            }
-            Ok(())
-        })?;
+//         let tile_size_px = self.tile_size_px;
+//         let offset_px = Vector::new(50, 120);
+//         let (tileset, map) = (&mut self.tileset, &self.map);
+//         tileset.execute(|tileset| {
+//             for tile in map.iter() {
+//                 if let Some(image) = tileset.get(&tile.glyph) {
+//                     let pos_px = tile.pos.times(tile_size_px);
+//                     window.draw(
+//                         &Rectangle::new(pos_px + offset_px, image.area().size()),
+//                         // Blended(&image, tile.color),
+//                     );
+//                 }
+//             }
+//             Ok(())
+//         })?;
 
-        let (tileset, entities) = (&mut self.tileset, &self.entities);
-        tileset.execute(|tileset| {
-            for entity in entities.iter() {
-                if let Some(image) = tileset.get(&entity.glyph) {
-                    let pos_px = offset_px + entity.pos.times(tile_size_px);
-                    window.draw(
-                        &Rectangle::new(pos_px, image.area().size()),
-                        Blended(&image, entity.color),
-                    );
-                }
-            }
-            Ok(())
-        })?;
+//         let (tileset, entities) = (&mut self.tileset, &self.entities);
+//         tileset.execute(|tileset| {
+//             for entity in entities.iter() {
+//                 if let Some(image) = tileset.get(&entity.glyph) {
+//                     let pos_px = offset_px + entity.pos.times(tile_size_px);
+//                     window.draw(
+//                         &Rectangle::new(pos_px, image.area().size()),
+//                         // Blended(&image, entity.color),
+//                     );
+//                 }
+//             }
+//             Ok(())
+//         })?;
 
-        let player = &self.entities[self.player_id];
-        let full_health_width_px = 100.0;
-        let current_health_width_px =
-            (player.hp as f32 / player.max_hp as f32) * full_health_width_px;
+//         let player = &self.entities[self.player_id];
+//         let full_health_width_px = 100.0;
+//         let current_health_width_px =
+//             (player.hp as f32 / player.max_hp as f32) * full_health_width_px;
 
-        let map_size_px = self.map_size.times(tile_size_px);
-        let health_bar_pos_px = offset_px + Vector::new(map_size_px.x, 0.0);
+//         let map_size_px = self.map_size.times(tile_size_px);
+//         let health_bar_pos_px = offset_px + Vector::new(map_size_px.x, 0.0);
 
-        window.draw(
-            &Rectangle::new(health_bar_pos_px, (full_health_width_px, tile_size_px.y)),
-            Col(Color::RED.with_alpha(0.5)),
-        );
-        window.draw(
-            &Rectangle::new(health_bar_pos_px, (current_health_width_px, tile_size_px.y)),
-            Col(Color::RED),
-        );
+//         // window.draw(
+//         //     &Rectangle::new(health_bar_pos_px, (full_health_width_px, tile_size_px.y)),
+//         //     Col(Color::RED.with_alpha(0.5)),
+//         // );
+//         // window.draw(
+//         //     &Rectangle::new(health_bar_pos_px, (current_health_width_px, tile_size_px.y)),
+//         //     Col(Color::RED),
+//         // );
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
 
 fn main() {
-    let settings = Settings {
-        scale: quicksilver::graphics::ImageScaleStrategy::Blur,
-        ..Default::default()
-    };
-    run::<Game>("Quicksilver Roguelike", Vector::new(800, 600), settings);
+    run(
+        Settings {
+            title: "Rust Roguelike",
+            ..Settings::default()
+        },
+        app,
+    );
+}
+
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    let mononoki = VectorFont::load("mononoki-Regular.ttf").await?;
+    let mut font = mononoki.to_renderer(&gfx, 16.0)?;
+    gfx.clear(Color::WHITE);
+
+    font.draw(
+        &mut gfx,
+        "Hello world!",
+        Color::BLACK,
+        Vector::new(100.0, 100.0),
+    )?;
+    gfx.present(&window)?;
+
+    loop {
+        while let Some(_) = input.next_event().await {}
+    }
 }
